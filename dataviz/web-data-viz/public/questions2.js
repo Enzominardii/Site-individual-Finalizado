1a9e1c4bcc8271b48aebc622b7c6feafbca53799
Enzominardii <enzo.azevedo@sptech.school>
2024-07-19 21:28:32 -0300

    Criando segundo questionario, atualizando menu lateral na tela da dashboard

---
+++ dataviz/web-data-viz/public/questions2.js
@@ -0,0 +1,86 @@
+
+// Sessao aonde sao colocadas as perguntas do quiz
+export default [
+    {
+      question: "Qual destas é uma marca famosa de raquetes de beach tennis?",
+      answers: [
+        { option: "A) Joma", correct: false },
+        { option: "B) Adidas", correct: false },
+        { option: "C) Reebok", correct: false },
+        { option: "D) Vision", correct: true },
+        { option: "E) Puma", correct: false },
+      ],
+    },
+    {
+      question: "Qual foi a primeira marca de beach tennis criada",
+      answers: [
+        { option: "A) Sandever", correct: true },
+        { option: "B) Adidas", correct: false },
+        { option: "C) Shark", correct: false },
+        { option: "D) Mormai", correct: false },
+        { option: "E) Nox", correct: false },
+      ],
+    },
+    {
+      question: "Qual marca Patrocina o Top 1 mundial (Nicolas Gianotti)",
+      answers: [
+        { option: "A) Heroes", correct: false },
+        { option: "B) Nox", correct: true },
+        { option: "C) Mormai", correct: false },
+        { option: "D) Quicksand", correct: false },
+        { option: "E) MBT", correct: false },
+      ],
+    },
+    {
+        question: "Qual marca Patrocina o Top 1 mundial (Matia Spotto)",
+        answers: [
+          { option: "A) Heroes", correct: true },
+          { option: "B) Nox", correct: false },
+          { option: "C) Mormai", correct: false },
+          { option: "D) Quicksand", correct: false },
+          { option: "E) MBT", correct: false },
+        ],
+      },
+    {
+      question: "Qual dessas marcas não foi criada no Brasil?",
+      answers: [
+        { option: "A) Amasport", correct: false },
+        { option: "B) Emit", correct: false },
+        { option: "C) Mormai", correct: false },
+        { option: "D) Zand", correct: false },
+        { option: "E) Kona", correct: true },
+      ],
+    },
+    {
+      question: "Qual marca é reconhecida por sua colaboração com jogadores profissionais para desenvolver raquetes como a (Spitfire)",
+      answers: [
+        { option: "A)  Shark", correct: false },
+        { option: "B) Quicksand", correct: true },
+        { option: "C) Nox", correct: false },
+        { option: "D) MBT", correct: false },
+        { option: "E) Drop Shot", correct: false },
+      ],
+    },
+    {
+      question: "Qual dessas marcas foi a criadora da raquete Zbruxo ultilizada pelo atual numero 5 do mundo Antomi Ramos",
+      answers: [
+        { option: "A) Zeiq", correct: false },
+        { option: "B) Zand", correct: true },
+        { option: "C) Rakketone", correct: false },
+        { option: "D) Head", correct: false },
+        { option: "E) Turquise", correct: false },
+      ],
+    },
+    {
+      question: " Qual dessas marcas oferece uma linha de raquetes com tecnologia de absorção de impacto para reduzir vibrações?",
+      answers: [
+        { option: "A) Yonex", correct: false },
+        { option: "B) Drop Shot", correct: true },
+        { option: "C) Head", correct: false },
+        { option: "D) Wilson", correct: false },
+        { option: "E) Babolat", correct: false },
+      ],
+    }
+  ];
+  
+  
