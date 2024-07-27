2064562fc6edb5d46f8719b02d77ce03141b013c
Enzominardii <enzo.azevedo@sptech.school>
2024-07-27 01:32:35 -0300

    Resolvendo problema do grafico, criando model,routes e controller

---
+++ dataviz/web-data-viz/src/controllers/quiz2Controller.js
@@ -0,0 +1,28 @@
+
+// quiz2 é aonde esta armazenando e mandando as informacoes do quiz para o banco de dados 
+
+var medidaModel = require("../models/medidaModel");
+
+function buscarPontuacoesPorQuiz(req, res) {
+    var Id_usuario = req.params.Id_usuario;
+
+    console.log(`Recuperando pontuações do quiz ${Id_usuario}`);
+
+    medidaModel.buscarPontuacoesPorQuiz(Id_usuario)
+        .then(function (resultado) {
+            if (resultado.length > 0) {
+                res.status(200).json(resultado);
+            } else {
+                res.status(204).send("Nenhuma pontuação encontrada!");
+            }
+        })
+        .catch(function (erro) {
+            console.log("Houve um erro ao buscar as pontuações:", erro);
+            res.status(500).json({ error: "Erro ao buscar pontuações do quiz" });
+        });
+}
+
+module.exports = {
+    buscarPontuacoesPorQuiz
+};
+
